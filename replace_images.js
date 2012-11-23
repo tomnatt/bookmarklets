06e38986029acf2bd689d7ff11ccd62dda79629a
tomnatt <github@tomnatt.com>
2012-11-23 16:52:24 +0000

    added silly image replace

---
+++ replace_images.js
@@ -0,0 +1,4 @@
+(function () {
+    for(var els = document.getElementsByTagName ('img'), i = els.length; i--;)
+        els[i].src = 'http://people.bath.ac.uk/ma1twn/images/surprise-man-computer.jpg';
+}) ();
